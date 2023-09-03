cb49ac908087b40c416db76b40bfbd5941ef1d51
Wagner Paulo <wagstalos@hotmail.com>
2023-09-03 17:02:01 -0300

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Magazine-Hashtag",
+  base: "/Magazine-Hashtag/",
   plugins: [],
   resolve: {
     /*something*/
